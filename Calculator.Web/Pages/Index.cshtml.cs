4a4bb0537fa02e1a6c387dc6c668eea34b3b4adf
Danil Safin <safindanil@yandex.ru>
2022-10-18 03:51:34 +0300

    Web application added

---
+++ Calculator.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Calculator.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private readonly ICalculator _calculator;
+
+        public IndexModel(ILogger<IndexModel> logger, ICalculator calculator)
+        {
+            _logger = logger;
+            _calculator = calculator;
+        }
+
+        [BindProperty]
+        public string? Expression { get; set; }
+
+        public string? Message { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || string.IsNullOrEmpty(Expression))
+            {
+                return Page();
+            }
+
+            try
+            {
+                Message = $"Результат: {_calculator.Calculate(Expression)}";
+            }
+            catch (ApplicationException e)
+            {
+                Message = e.Message;
+            }
+
+            return Page();
+        }
+    }
+}
