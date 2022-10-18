fa66bbc32e5132cd0cc4e9af069cedab434c1725
Danil Safin <safindanil@yandex.ru>
2022-10-18 03:52:11 +0300

    Console application added

---
+++ CalcConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculator.Impl;
+
+start:
+Console.Write("Введите выражение для вычисления: ");
+var expr = Console.ReadLine();
+if (string.IsNullOrEmpty(expr))
+{
+    goto start;
+}
+var calculator = new CalculatorImpl(expr => new TokensEnumerator(new TokensEnumeratorContext(expr), new TokensFactory()));
+
+Console.Write("Результат: ");
+Console.WriteLine(calculator.Calculate(expr));
+
+Console.WriteLine("Нажмите любую кладвишу для продолжения...");
+Console.ReadKey();
